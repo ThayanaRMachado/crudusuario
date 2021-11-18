9ec65d79838f04e19f381cf20b0dadae68a34070
ThayanaRMachado <thayanarm@hotmail.com>
2021-11-18 17:06:00 -0300

    Project created

---
+++ src/test/java/com/bootcampcrudusuario/CrudusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampcrudusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
